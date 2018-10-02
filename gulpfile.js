578ab235648f2fdbaf21f041c4dafc99c325ecb7
necron9x11 <necron9x11@protonmail.com>
2018-10-02 05:07:21 -0500

    saftey save of current work

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     postcss = require('gulp-postcss'),
-    autoprefixer = require('autoprefixer');
+    autoprefixer = require('autoprefixer'),
+    cssvars = require('postcss-simple-vars'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log("Hurray - you created a Gulp task");
@@ -13,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   //  console.log("Imagine Sass or PostCSS tasks running here.");
-  return gulp.src('./app/assets/styles/styles.css').pipe(postcss([autoprefixer])).pipe(gulp.dest('./app/temp/styles.css'));
+  return gulp.src('./app/assets/styles/styles.css').pipe(postcss([cssImport, cssvars, nested, autoprefixer])).pipe(gulp.dest('./app/temp/styles'));
 });
 
 //   return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles.css'));
